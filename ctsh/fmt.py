c4d2bb442bc85a7a1c6e3d17274004673d560e31
blueloveTH <blueloveTH@foxmail.com>
2023-08-19 21:31:32 +0800

    ...

--- ctsh/fmt.py
+++ ctsh/fmt.py
@@ -1,11 +1,18 @@
+def rgb(r: int, g: int, b: int, s: str) -> str:
+    return f'\033[38;2;{r};{g};{b}m{s}\033[0m'
+
+
+def rgb_bg(r: int, g: int, b: int, s: str) -> str:
+    return f'\033[48;2;{r};{g};{b}m{s}\033[0m'
+
+def blue(s: str) -> str:
+    return rgb(0, 192, 255, s)
+
+def green(s: str) -> str:
+    return rgb(0, 255, 0, s)
+
 def bold(s: str) -> str:
     return f'\033[1m{s}\033[0m'
 
 def gray(s: str) -> str:
     return f'\033[90m{s}\033[0m'
-
-def blue(s: str) -> str:
-    return f'\033[94m{s}\033[0m'
-
-def green(s: str) -> str:
-    return f'\033[92m{s}\033[0m'
